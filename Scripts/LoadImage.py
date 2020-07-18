94a38406c00774f60e6ecf3be0651045afd623ef
RisithPerera <risithperera@eng.pdn.ac.lk>
2020-07-18 21:01:34 +0530

    Started Face Detection

---
+++ Scripts/LoadImage.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import glob
+
+image = cv.imread('profile.jpg', 0)
+
+#print(type(image))
+#print(image)
+#print(image)
+#print(image.shape)
+#print(image.ndim)
+
+devide = 2
+resized_image = cv.resize(image,( int(image.shape[1]/devide),int(image.shape[0]/devide)))
+cv.imshow("Galaxy", resized_image)
+cv.imwrite("Galaxy_Resized.jpg", resized_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
---
+++ Scripts/batchImageResize.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import glob
+
+images = glob.glob("*.jpg")
+
+for imageName in images:
+    image = cv.imread(imageName, 0)
+    re = cv.resize(image, (100,100))
+    cv.imshow("image", re)
+    cv.waitKey(500)
+    cv.destroyAllWindows()
+    cv.imwrite("resized_"+imageName+".jpg", re)
+
+
