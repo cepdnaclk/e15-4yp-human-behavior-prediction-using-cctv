7a6a6703463d86ad0b7dce55cfd13dc4ec3e74e4
RisithPerera <risithperera@eng.pdn.ac.lk>
2020-07-25 14:03:27 +0530

    Motion Detection using basic image manipulations.

---
+++ Scripts/detectMovingObjects.py
@@ -0,0 +1,53 @@
+import cv2 as cv
+import pandas as pd
+from datetime import datetime
+
+frameRate = 24  # fps
+firstFrame = None
+video = cv.VideoCapture(0)
+faceCascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+df = pd.DataFrame(columns=["Start", "End"])
+startTime = None
+
+while True:
+    check, currFrame = video.read()
+    currStatus = 0
+    editFrame = cv.cvtColor(currFrame, cv.COLOR_BGR2GRAY)
+    editFrame = cv.GaussianBlur(editFrame, (21, 21), 0)
+
+    if firstFrame is None:
+        preStatus = currStatus
+        firstFrame = editFrame
+        continue
+
+    editFrame = cv.absdiff(editFrame, firstFrame)
+    editFrame = cv.threshold(editFrame, 30, 255, cv.THRESH_BINARY)[1]
+    editFrame = cv.dilate(editFrame, None, iterations= 2)
+    
+    (contours, _) = cv.findContours(editFrame.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv.contourArea(contour) > 10000:
+            currStatus = 1
+            (x,y,w,h) = cv.boundingRect(contour)
+            dilateFrame = cv.rectangle(currFrame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+
+    if preStatus == 0 and currStatus == 1:
+        startTime = datetime.now()
+    elif preStatus == 1 and currStatus == 0:
+        df = df.append({"Start": startTime, "End": datetime.now()}, ignore_index=True)
+
+    cv.imshow("Current_Frame", currFrame)
+    cv.imshow("Edit_Frame", editFrame)
+    key = cv.waitKey(int(1000/frameRate))
+
+    if key == ord('z'):
+        if currStatus == 1:
+            df = df.append({"Start": startTime, "End": datetime.now()}, ignore_index=True)
+        break
+
+    preStatus = currStatus
+
+df.to_csv("Times.csv")
+video.release()
+cv.destroyAllWindows()
