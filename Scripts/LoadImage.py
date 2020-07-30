9297e780941f1398d02391b393578fe29aafcfa8
RisithPerera <risithperera@eng.pdn.ac.lk>
2020-07-30 18:02:50 +0530

    Advanced image manipulation

--- Scripts/LoadImage.py
+++ Scripts/LoadImage.py
@@ -1,17 +1,45 @@
 import cv2 as cv
-import glob
+import numpy as np
 
-image = cv.imread('../Images/profile.jpg', 0)
+#print(cv.__version__)
+image = cv.imread('../Images/Lighthouse.jpg')
+hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+B, G, R = cv.split(image)
 
-#print(type(image))
-#print(image)
-#print(image)
-#print(image.shape)
-#print(image.ndim)
+zeros = np.zeros(image.shape[:2], dtype='uint8')
+fills = 255* np.ones(image.shape[:2], dtype='uint8')
 
-devide = 2
+intensity = 40
+
+BNoise = np.uint8(np.random.randint(0, 255-intensity, size=(image.shape[:2])))
+GNoise = np.uint8(np.random.randint(0, 255-intensity, size=(image.shape[:2])))
+RNoise = np.uint8(np.random.randint(0, 255-intensity, size=(image.shape[:2])))
+
+BMask = (B < intensity).astype(int)
+GMask = (G < intensity).astype(int)
+RMask = (R < intensity).astype(int)
+
+BNoise = np.uint8(np.multiply(BNoise, BMask))
+GNoise = np.uint8(np.multiply(GNoise, GMask))
+RNoise = np.uint8(np.multiply(RNoise, RMask))
+
+B = np.uint8(np.add(B, BNoise))
+G = np.uint8(np.add(G, GNoise))
+R = np.uint8(np.add(R, RNoise))
+
+editFrame = cv.merge([B,G,R])
+#editFrame = cv.GaussianBlur(editFrame, (5, 5), 0)
+cv.imshow("HSV", hsv_image)
+cv.imshow("H", hsv_image[:, :, 0])
+cv.imshow("S", hsv_image[:, :, 1])
+cv.imshow("V", hsv_image[:, :, 2])
+#cv.imshow("Blue", cv.merge([B,zeros,zeros]))
+#cv.imshow("Green", G)
+#cv.imshow("Red", R)
+
+devide = 1
 resized_image = cv.resize(image,( int(image.shape[1]/devide),int(image.shape[0]/devide)))
-cv.imshow("Galaxy", resized_image)
-cv.imwrite("Galaxy_Resized.jpg", resized_image)
+#cv.imshow("Galaxy", resized_image)
+#cv.imwrite("Galaxy_Resized.jpg", resized_image)
 cv.waitKey(0)
 cv.destroyAllWindows()
--- Scripts/videoCapture.py
+++ Scripts/videoCapture.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 import time
 
 video = cv.VideoCapture(0)
@@ -7,13 +8,33 @@ faceCascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while True:
     check, currFrame = video.read()
+    B, G, R = cv.split(currFrame)
+
+    intensity = 150
+    BNoise = np.uint8(np.random.randint(0, 255-intensity, size=(currFrame.shape[:2])))
+    GNoise = np.uint8(np.random.randint(0, 255-intensity, size=(currFrame.shape[:2])))
+    RNoise = np.uint8(np.random.randint(0, 255-intensity, size=(currFrame.shape[:2])))
+
+    BMask = (B < intensity).astype(int)
+    GMask = (G < intensity).astype(int)
+    RMask = (R < intensity).astype(int)
+
+    BNoise = np.uint8(np.multiply(BNoise, BMask))
+    GNoise = np.uint8(np.multiply(GNoise, GMask))
+    RNoise = np.uint8(np.multiply(RNoise, RMask))
+
+    B = np.uint8(np.add(B, BNoise))
+    G = np.uint8(np.add(G, GNoise))
+    R = np.uint8(np.add(R, RNoise))
+
+    editFrame = cv.merge([B, G, R])
     greyFrame = cv.cvtColor(currFrame, cv.COLOR_BGR2GRAY)
+
     faces = faceCascade.detectMultiScale(greyFrame, scaleFactor=1.1, minNeighbors=5)
     for x, y, w, h, in faces:
         currFrame = cv.rectangle(currFrame, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
-
-    cv.imshow("Web Cam", currFrame)
+    cv.imshow("Web Cam", editFrame)
     key = cv.waitKey(int(1000/frameRate))
 
     if key == ord('z'):
