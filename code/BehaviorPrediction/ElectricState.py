123a4976eefaa90313f21e8a207b5af0304eec5c
RisithPerera <risithperera@eng.pdn.ac.lk>
2021-04-18 22:51:16 +0530

    Move Files Reattempt

--- code/BehaviorPrediction/ElectricState.py
+++ code/BehaviorPrediction/ElectricState.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  5 21:32:17 2021
-
-@author: Acer
-"""
-
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from catboost import CatBoostClassifier,Pool
